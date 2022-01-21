a990a21fb0d26fbd635276d994d796f1f9694b1e
A Nefedov <alexandr.nefedov.b2w@gmail.com>
2022-01-21 14:39:46 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# This repo contains 5 ansible roles (all of them for CentOS):
+1. Update packages;
+2. Disable firewalld and selinux;
+3. Install net-tools, telnet, nano, mc, vim
+4. Install Python 3.9.10
+5. Install Docker
---
+++ _run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+ansible()
+{
+	ansible-playbook playbook.yml
+}
+
+if ((`wc -c < ansible.log` > 51200)); then
+	echo "Attention! Ansible log is more then 50kb.
+	0) Clear ansible.log and continue.
+	1) Ignore and continue.
+	Any other key) Exit."
+	read -p "Choose your action [0-2]:" x
+	case $x in
+		0) echo > ansible.log 
+		ansible;;
+		1) ansible;;
+		*) exit
+	esac
+else ansible
+fi
